3a078ebd907cccd6195253455ef9fae4911e08b2
MenalKour <menalkaur2003@gmail.com>
2022-09-22 19:28:11 +0530

    second commit

---
+++ public/script.js
@@ -0,0 +1,51 @@
+const socket = io("/");
+
+var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "443",
+});
+
+const user = prompt("Enter your name");
+
+$(function () {
+    $("#show_chat").click(function () {
+        $(".left-window").css("display", "none")
+        $(".right-window").css("display", "block")
+        $(".header_back").css("display", "block")
+    })
+    $(".header_back").click(function () {
+        $(".left-window").css("display", "block")
+        $(".right-window").css("display", "none")
+        $(".header_back").css("display", "none")
+    })
+
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+})
+
+peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id, user);
+});
+
+socket.on("createMessage", (message, userName) => {
+    $(".messages").append(`
+        <div class="message">
+            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
+        }</span> </b>
+            <span>${message}</span>
+        </div>
+    `)
+});
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const { v4: uuidv4 } = require("uuid");
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+
+app.use("/peerjs", peerServer);
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+    });
+});
+
+server.listen(3030);
